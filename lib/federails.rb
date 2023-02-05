98c8081878ea44c6ba7d7564602d8b6408058cc6
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-05 07:43:57 +0100

    chore(project): Make factories available to main app

--- lib/federails.rb
+++ lib/federails.rb
@@ -2,5 +2,6 @@ require 'federails/version'
 require 'federails/engine'
 
 module Federails
-  # Your code goes here...
+  # Make factories available
+  config.factory_bot.definition_file_paths += [File.expand_path('spec/factories', __dir__)] if defined?(FactoryBotRails)
 end
