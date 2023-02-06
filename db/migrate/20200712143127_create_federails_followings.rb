6efccba18ff29d25218eaca004d274347807f781
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-06 21:17:30 +0100

    fix(migrations): Update followings table creation

--- db/migrate/20200712143127_create_federails_followings.rb
+++ db/migrate/20200712143127_create_federails_followings.rb
@@ -1,8 +1,8 @@
 class CreateFederailsFollowings < ActiveRecord::Migration[7.0]
   def change
-    create_table :followings do |t|
-      t.references :actor, null: false, foreign_key: true
-      t.references :target_actor, null: false, foreign_key: { to_table: :actors }
+    create_table :federails_followings do |t|
+      t.references :actor, null: false, foreign_key: { to_table: :federails_actors }
+      t.references :target_actor, null: false, foreign_key: { to_table: :federails_actors }
       t.integer :status, default: 0
       t.string :federated_url
 
