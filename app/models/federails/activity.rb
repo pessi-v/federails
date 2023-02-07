5ad67c04f1009dd30b7ddb3b85ec7ee4ca221be3
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-07 08:11:21 +0100

    fix(models): Namespace in Federails module

--- app/models/federails/activity.rb
+++ app/models/federails/activity.rb
@@ -1,37 +1,39 @@
-class Activity < ApplicationRecord
-  belongs_to :entity, polymorphic: true
-  belongs_to :actor
+module Federails
+  class Activity < ApplicationRecord
+    belongs_to :entity, polymorphic: true
+    belongs_to :actor
 
-  scope :feed_for, lambda { |actor|
-    actor_ids = []
-    Following.accepted.where(actor: actor).find_each do |following|
-      actor_ids << following.target_actor_id
+    scope :feed_for, lambda { |actor|
+      actor_ids = []
+      Following.accepted.where(actor: actor).find_each do |following|
+        actor_ids << following.target_actor_id
+      end
+      where(actor_id: actor_ids)
+    }
+
+    after_create_commit :post_to_inboxes
+
+    def recipients # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
+      return [] unless actor.local?
+
+      actors = []
+      case action
+      when 'Create'
+        actors.push(entity.target_actor) if entity_type == 'Following'
+        actors.push(*actor.followers) if entity_type == 'Note'
+      when 'Accept'
+        actors.push(entity.actor) if entity_type == 'Following'
+      when 'Undo'
+        actors.push(entity.target_actor) if entity_type == 'Following'
+      end
+
+      actors
     end
-    where(actor_id: actor_ids)
-  }
 
-  after_create_commit :post_to_inboxes
+    private
 
-  def recipients # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
-    return [] unless actor.local?
-
-    actors = []
-    case action
-    when 'Create'
-      actors.push(entity.target_actor) if entity_type == 'Following'
-      actors.push(*actor.followers) if entity_type == 'Note'
-    when 'Accept'
-      actors.push(entity.actor) if entity_type == 'Following'
-    when 'Undo'
-      actors.push(entity.target_actor) if entity_type == 'Following'
+    def post_to_inboxes
+      NotifyInboxJob.perform_later(self)
     end
-
-    actors
-  end
-
-  private
-
-  def post_to_inboxes
-    NotifyInboxJob.perform_later(self)
   end
 end
--- spec/models/federails/activity_spec.rb
+++ spec/models/federails/activity_spec.rb
@@ -1,37 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe Activity, type: :model do
-  let(:actor) { FactoryBot.create(:user, :confirmed).actor }
-  let(:distant_actor) { FactoryBot.create :actor, :distant }
-  let(:following) { FactoryBot.create :following, actor: actor, target_actor: distant_actor }
-  let(:distant_following) { FactoryBot.create :following, actor: distant_actor, target_actor: actor }
+module Federails
+  RSpec.describe Activity, type: :model do
+    let(:actor) { FactoryBot.create(:user, :confirmed).actor }
+    let(:distant_actor) { FactoryBot.create :actor, :distant }
+    let(:following) { FactoryBot.create :following, actor: actor, target_actor: distant_actor }
+    let(:distant_following) { FactoryBot.create :following, actor: distant_actor, target_actor: actor }
 
-  describe '.recipients' do
-    context 'when activity creator is distant' do
-      it 'does not notify actor' do
-        activity = described_class.new actor: distant_actor
-        expect(activity.recipients).to eq []
+    describe '.recipients' do
+      context 'when activity creator is distant' do
+        it 'does not notify actor' do
+          activity = described_class.new actor: distant_actor
+          expect(activity.recipients).to eq []
+        end
       end
-    end
 
-    context 'when creating a Following' do
-      it 'notifies the target actor' do
-        expect(following.activities.last.recipients).to eq [distant_actor]
+      context 'when creating a Following' do
+        it 'notifies the target actor' do
+          expect(following.activities.last.recipients).to eq [distant_actor]
+        end
       end
-    end
 
-    context 'when accepting a Following' do
-      it 'notifies the Following creator' do
-        distant_following.accept!
-        expect(distant_following.activities.last.recipients).to eq [distant_actor]
+      context 'when accepting a Following' do
+        it 'notifies the Following creator' do
+          distant_following.accept!
+          expect(distant_following.activities.last.recipients).to eq [distant_actor]
+        end
       end
-    end
 
-    context 'when creating a Note' do
-      it 'notifies the followers' do
-        distant_following.accept!
-        note = FactoryBot.create :note, actor: actor
-        expect(note.activities.last.recipients).to eq [distant_actor]
+      context 'when creating a Note' do
+        it 'notifies the followers' do
+          distant_following.accept!
+          note = FactoryBot.create :note, actor: actor
+          expect(note.activities.last.recipients).to eq [distant_actor]
+        end
       end
     end
   end
