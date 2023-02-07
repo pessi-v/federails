870d2d35720ae508b56b842fbd9dc11b60af019b
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-07 08:35:39 +0100

    fix(factories): Load factories from engine

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Load engine factories
+FactoryBot.definition_file_paths << Federails::Engine.root.join('spec', 'factories')
+FactoryBot.reload
