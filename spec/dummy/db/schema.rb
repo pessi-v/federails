5f2a27c01d5488d486e1b8c945b52c546eebbc71
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-07 09:17:14 +0100

    chore(dummy): Update database schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_05_081230) do
+ActiveRecord::Schema[7.0].define(version: 2020_07_12_174938) do
+  create_table "federails_activities", force: :cascade do |t|
+    t.string "entity_type", null: false
+    t.integer "entity_id", null: false
+    t.string "action", null: false
+    t.integer "actor_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["actor_id"], name: "index_federails_activities_on_actor_id"
+    t.index ["entity_type", "entity_id"], name: "index_federails_activities_on_entity"
+  end
+
+  create_table "federails_actors", force: :cascade do |t|
+    t.string "name"
+    t.string "federated_url"
+    t.string "username"
+    t.string "server"
+    t.string "inbox_url"
+    t.string "outbox_url"
+    t.string "followers_url"
+    t.string "followings_url"
+    t.string "profile_url"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["federated_url"], name: "index_federails_actors_on_federated_url", unique: true
+    t.index ["user_id"], name: "index_federails_actors_on_user_id", unique: true
+  end
+
+  create_table "federails_followings", force: :cascade do |t|
+    t.integer "actor_id", null: false
+    t.integer "target_actor_id", null: false
+    t.integer "status", default: 0
+    t.string "federated_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["actor_id", "target_actor_id"], name: "index_federails_followings_on_actor_id_and_target_actor_id", unique: true
+    t.index ["actor_id"], name: "index_federails_followings_on_actor_id"
+    t.index ["target_actor_id"], name: "index_federails_followings_on_target_actor_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_05_081230) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "federails_activities", "federails_actors", column: "actor_id"
+  add_foreign_key "federails_actors", "users"
+  add_foreign_key "federails_followings", "federails_actors", column: "actor_id"
+  add_foreign_key "federails_followings", "federails_actors", column: "target_actor_id"
 end
