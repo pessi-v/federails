45eea2bebac8058cd2c75e02f50ab7cafe99bc3c
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-08 16:02:40 +0100

    chore(routes): Import federated routes under a configurable scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Federails::Engine.routes.draw do
-  # Routes goes here
+  scope Federails.configuration.routes_path do
+    resources :actors, only: [:show] do
+      member do
+        get :followers
+        get :following
+      end
+      get :outbox, to: 'activities#outbox'
+      post :inbox, to: 'activities#create'
+      resources :activities, only: [:show]
+      resources :followings, only: [:show]
+      resources :notes, only: [:show]
+    end
+  end
 end
