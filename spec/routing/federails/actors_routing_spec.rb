000c3e8a7572b3dd909dac90d3d641d94d24dfbe
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-12 13:52:44 +0100

    feat(tests): Create controller routing specs

---
+++ spec/routing/federails/actors_routing_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+module Federails
+  RSpec.describe ActorsController, type: :routing do
+    describe 'routing' do
+      it 'routes to #show' do
+        expect(get: '/federails/federation/actors/1').to route_to('federails/actors#show', id: '1')
+      end
+
+      it 'routes to #followers' do
+        expect(get: '/federails/federation/actors/1/followers').to route_to('federails/actors#followers', id: '1')
+      end
+
+      it 'routes to #following' do
+        expect(get: '/federails/federation/actors/1/following').to route_to('federails/actors#following', id: '1')
+      end
+    end
+  end
+end
---
+++ spec/routing/federails/followings_routing_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+module Federails
+  RSpec.describe FollowingsController, type: :routing do
+    describe 'routing' do
+      it 'routes to #show' do
+        expect(get: '/federails/federation/actors/1/followings/2').to route_to('federails/followings#show', actor_id: '1', id: '2')
+      end
+    end
+  end
+end
