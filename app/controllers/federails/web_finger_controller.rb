e153ff3110c8057ae871e33c37e7afc3ec2f0e22
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-12 14:44:31 +0100

    chore(controllers): Import "web finger" from proof of concept

---
+++ app/controllers/federails/web_finger_controller.rb
@@ -0,0 +1,33 @@
+require 'fediverse/webfinger'
+
+class WebFingerController < ApplicationController
+  def find
+    @user = User.find_by! username: username
+    render formats: [:json]
+  end
+
+  def host_meta
+    render content_type: 'application/xrd+xml', formats: [:xml]
+  end
+
+  def node_info
+    render formats: [:json]
+  end
+
+  # FIXME: Move this action in another controller; it does not belong to webfinger
+  def show_node_info
+    render formats: [:json]
+  end
+
+  # TODO: complete missing endpoints
+
+  private
+
+  def username
+    account = Fediverse::Webfinger.split_resource_account params.require(:resource)
+    # Fail early if user don't _seems_ local
+    raise ActiveRecord::RecordNotFound unless account && Fediverse::Webfinger.local_user?(account)
+
+    account[:username]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Federails::Engine.routes.draw do
+  get '/.well-known/webfinger', to: 'web_finger#find', as: :webfinger
+  get '/.well-known/host-meta', to: 'web_finger#host_meta', as: :host_meta
+  get '/.well-known/nodeinfo', to: 'web_finger#node_info', as: :node_info
+  get '/nodeinfo/2.0', to: 'web_finger#show_node_info', as: :show_node_info
+
   scope Federails.configuration.routes_path do
     resources :actors, only: [:show] do
       member do
