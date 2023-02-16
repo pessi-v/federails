6028eaa039faf18b722149f19f9727c0452027db
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-16 10:16:02 +0100

    feat(configuration): Set routes default URL options from configuration

--- lib/federails/configuration.rb
+++ lib/federails/configuration.rb
@@ -4,10 +4,10 @@ module Federails
     mattr_accessor :force_ssl
     @@force_ssl = nil
 
-    mattr_accessor :site_host
+    mattr_reader :site_host
     @@site_host = nil
 
-    mattr_accessor :site_port
+    mattr_reader :site_port
     @@site_port = nil
 
     mattr_accessor :user_class
@@ -31,6 +31,16 @@ module Federails
       raise 'User class is not defined' unless @@user_class
 
       @@user_class.constantize
+    end
+
+    def self.site_host=(value)
+      @@site_host = value
+      Federails::Engine.routes.default_url_options[:host] = value
+    end
+
+    def self.site_port=(value)
+      @@site_port = value
+      Federails::Engine.routes.default_url_options[:port] = value
     end
   end
   # rubocop:enable Style/ClassVars
