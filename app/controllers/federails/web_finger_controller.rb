613ab753b6550787bdf59ed69b20417f7238d8ee
Manuel Tancoigne <manu@experimentslabs.com>
2023-02-16 10:12:08 +0100

    fix(controllers): Namespace webfinger in Federails module

--- app/controllers/federails/web_finger_controller.rb
+++ app/controllers/federails/web_finger_controller.rb
@@ -1,33 +1,35 @@
 require 'fediverse/webfinger'
 
-class WebFingerController < ApplicationController
-  def find
-    @user = User.find_by! username: username
-    render formats: [:json]
-  end
+module Federails
+  class WebFingerController < ApplicationController
+    def find
+      @user = User.find_by! username: username
+      render formats: [:json]
+    end
 
-  def host_meta
-    render content_type: 'application/xrd+xml', formats: [:xml]
-  end
+    def host_meta
+      render content_type: 'application/xrd+xml', formats: [:xml]
+    end
 
-  def node_info
-    render formats: [:json]
-  end
+    def node_info
+      render formats: [:json]
+    end
 
-  # FIXME: Move this action in another controller; it does not belong to webfinger
-  def show_node_info
-    render formats: [:json]
-  end
+    # FIXME: Move this action in another controller; it does not belong to webfinger
+    def show_node_info
+      render formats: [:json]
+    end
 
-  # TODO: complete missing endpoints
+    # TODO: complete missing endpoints
 
-  private
+    private
 
-  def username
-    account = Fediverse::Webfinger.split_resource_account params.require(:resource)
-    # Fail early if user don't _seems_ local
-    raise ActiveRecord::RecordNotFound unless account && Fediverse::Webfinger.local_user?(account)
+    def username
+      account = Fediverse::Webfinger.split_resource_account params.require(:resource)
+      # Fail early if user don't _seems_ local
+      raise ActiveRecord::RecordNotFound unless account && Fediverse::Webfinger.local_user?(account)
 
-    account[:username]
+      account[:username]
+    end
   end
 end
