7678eaac902ee88230d1cc2852157c9984a9236d
Manuel Tancoigne <manu@experimentslabs.com>
2023-11-10 08:57:55 +0100

    fix(db): Update schema for Rails 7.1

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2020_07_12_174938) do
+ActiveRecord::Schema[7.1].define(version: 2020_07_12_174938) do
   create_table "federails_activities", force: :cascade do |t|
     t.string "entity_type", null: false
     t.integer "entity_id", null: false
